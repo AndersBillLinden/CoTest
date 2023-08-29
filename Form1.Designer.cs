6e8500289592a268680c63a6e678c29b2ecee225
Anders Lindén <purist.coder@gmail.com>
2023-08-29 14:33:22 +0200

    Bug reproduction

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TreeViewAdvBugRepro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Syncfusion.Windows.Forms.Tools.TreeNodeAdvStyleInfo treeNodeAdvStyleInfo1 = new Syncfusion.Windows.Forms.Tools.TreeNodeAdvStyleInfo();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeViewAdv1 = new Syncfusion.Windows.Forms.Tools.TreeViewAdv();
+            ((System.ComponentModel.ISupportInitialize)(this.treeViewAdv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "16_plus.png");
+            // 
+            // treeViewAdv1
+            // 
+            treeNodeAdvStyleInfo1.CheckBoxTickThickness = 1;
+            treeNodeAdvStyleInfo1.CheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            treeNodeAdvStyleInfo1.EnsureDefaultOptionedChild = true;
+            treeNodeAdvStyleInfo1.IntermediateCheckColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            treeNodeAdvStyleInfo1.OptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            treeNodeAdvStyleInfo1.SelectedOptionButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.treeViewAdv1.BaseStylePairs.AddRange(new Syncfusion.Windows.Forms.Tools.StyleNamePair[] {
+            new Syncfusion.Windows.Forms.Tools.StyleNamePair("Standard", treeNodeAdvStyleInfo1)});
+            this.treeViewAdv1.BeforeTouchSize = new System.Drawing.Size(312, 368);
+            // 
+            // 
+            // 
+            this.treeViewAdv1.HelpTextControl.BaseThemeName = null;
+            this.treeViewAdv1.HelpTextControl.Location = new System.Drawing.Point(0, 0);
+            this.treeViewAdv1.HelpTextControl.Name = "";
+            this.treeViewAdv1.HelpTextControl.Size = new System.Drawing.Size(392, 112);
+            this.treeViewAdv1.HelpTextControl.TabIndex = 0;
+            this.treeViewAdv1.HelpTextControl.Visible = true;
+            this.treeViewAdv1.InactiveSelectedNodeForeColor = System.Drawing.SystemColors.ControlText;
+            this.treeViewAdv1.Location = new System.Drawing.Point(13, 13);
+            this.treeViewAdv1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+            this.treeViewAdv1.Name = "treeViewAdv1";
+            this.treeViewAdv1.SelectedNodeForeColor = System.Drawing.SystemColors.HighlightText;
+            this.treeViewAdv1.Size = new System.Drawing.Size(312, 368);
+            this.treeViewAdv1.TabIndex = 0;
+            this.treeViewAdv1.Text = "treeViewAdv1";
+            // 
+            // 
+            // 
+            this.treeViewAdv1.ToolTipControl.BaseThemeName = null;
+            this.treeViewAdv1.ToolTipControl.Location = new System.Drawing.Point(0, 0);
+            this.treeViewAdv1.ToolTipControl.Name = "";
+            this.treeViewAdv1.ToolTipControl.Size = new System.Drawing.Size(392, 112);
+            this.treeViewAdv1.ToolTipControl.TabIndex = 0;
+            this.treeViewAdv1.ToolTipControl.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 400);
+            this.Controls.Add(this.treeViewAdv1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.treeViewAdv1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private Syncfusion.Windows.Forms.Tools.TreeViewAdv treeViewAdv1;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TreeViewAdvBugRepro
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NGaF1cVGhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEZjUX1ccHFURGFbUUd3XA==");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
